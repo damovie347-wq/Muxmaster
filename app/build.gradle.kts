7ba6ea41243aa3115265033fe3c3f6a22bb4eeef
damovie347-wq <damovie347@gmail.com>
2026-07-03 10:50:50 +0300

    build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,11 +19,13 @@ android {
     }
 
     buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
+    release {
+        signingConfig = signingConfigs.getByName("debug")
+        isMinifyEnabled = true
+        isShrinkResources = true
+        proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
     }
+}
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
