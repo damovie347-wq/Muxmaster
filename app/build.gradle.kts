ea7b57f0bfc224743de0d553e246d0c2ecb73ca1
damovie347-wq <damovie347@gmail.com>
2026-07-06 00:28:10 +0300

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
     implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.documentfile:documentfile:1.0.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation("com.moizhassan.ffmpeg:ffmpeg-kit-16kb:6.1.1")
